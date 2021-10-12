1515186cc1dcf8de0b9aa5fe76095c74ed7c1210
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2021-10-12 12:59:56 +0200

    init module creation navbar

---
+++ Class/Files_name.php
@@ -0,0 +1,10 @@
+<?php
+    class Files_name {
+        public static $brand_logo   = 'design/fcb.svg';
+        public static $page_index   = ['index.php', 'home'];
+        public static $page_index2  = ['index2.php', 'page2'];
+        public static $page_index3  = ['index3.php', 'page3'];
+        // public static $page_index4  = ['index4.php', 'page4'];
+        // public static $page_index5  = ['index5.php', 'page5'];
+        // public static $page_index6  = ['index6.php', 'page6'];
+    }
